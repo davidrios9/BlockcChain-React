e8e18d8b7b5f0f7f247186fa2d236796c9741b13
Nicolás F. Jaramillo <nf.jaramillo@uniandes.edu.co>
2023-04-04 09:54:04 -0500

    Updates on new document

--- src/pages/nuevoDoc.jsx
+++ src/pages/nuevoDoc.jsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Grid, Typography, TextField, Autocomplete } from "@mui/material"
-
+import { useContext } from "react";
+import { Grid, Typography, TextField, Autocomplete, Button } from "@mui/material";
+import { AppContext } from '../App.jsx';
 
 const NuevoDoc = (props) => {
 
@@ -18,24 +19,63 @@ const NuevoDoc = (props) => {
         { label: 'T160556', name: 'Abrir productos Factoring' }
     ]
 
+    const lideres = [
+        { label: 'LACARDON', name: 'CARDONA GARZON LUZ ADRIANA' },
+        { label: 'ELGIRALD', name: 'GIRALDO DUQUE ELIANA MARIA' },
+        { label: 'JCHIN', name: 'CHIN DEGENHARDT JOSE ROLANDO' },
+        { label: 'LUZRAMOS', name: 'RAMOS AMAYA LUZ ADRIANA' },
+        { label: 'MLANCHER', name: 'LANCHEROS ALVAREZ MAURICIO ANDRES' },
+        { label: 'MAVARGAS', name: 'VARGAS MADRIGAL LINA MARCELA' },
+        { label: 'APCASTRI', name: 'CASTRILLON ARENAS ANGELA PATRICIA' },
+        { label: 'MMGUTIER', name: 'GUTIERREZ BEDOYA MONICA MARIA' },
+        { label: 'JUEMOREN', name: 'MORENO CARDONA JUAN ESTEBAN' },
+        { label: 'JPCASTAN', name: 'CASTAÑO ALVAREZ JUAN PABLO' }
+    ]
+
+    const documentos = [
+        { label: 'BPM (editable bizagi)' },
+        { label: 'BPM (Visor)' },
+        { label: 'Instructivos' },
+        { label: 'Anexos' },
+        { label: 'Otros' }
+    ]
+
     const [proceso, setProceso] = React.useState('Escoja un proceso');
+    const [lider, setLider] = React.useState('Escoja un lider aprobador');
+    const [justificacion, setJustificacion] = React.useState('');
+    const contextData = useContext(AppContext);
+
+    const handleApplication = async () => {
+        if (proceso !== 'Escoja un proceso' && lider !== 'Escoja un lider aprobador' && justificacion !== '') {
+
+            contextData.severity("success")
+            contextData.text("Solicitud radicada");
+            contextData.show(true)
+        }
+        else {
+            contextData.severity("warning")
+            contextData.text("Llene todos los campos");
+            contextData.show(true)
+        }
+
+    }
 
     return (
         <Grid container rowSpacing={1} padding={0} justifyContent="center" align='center' maxWidth="xl" direction={{ xs: 'column', md: 'row' }} sx={{ display: 'flex', bgcolor: '#cfe8fc', minHeight: '80vh', borderRadius: 1, mt: 3, background: 'linear-gradient(to bottom, #F8F8F8, #FFFFFF)' }}>
-            <Grid item display='flex' alignItems='center' justifyContent='center'  xs={12} >
-                <Typography sx={{ mt: 3, typography: { xs: 'h5', sm: 'h5', md: 'h2', lg: 'h2' } }}>Nuevo documento</Typography>
+            <Grid item display='flex' alignItems='center' justifyContent='center' xs={12} >
+                <Typography sx={{ typography: { xs: 'h5', sm: 'h5', md: 'h2', lg: 'h2' } }}>Nuevo documento</Typography>
             </Grid>
-            <Grid item  xs={5} sx={{ width: '95vw', height: '10vh', backgroundColor: 'primary.main', borderRadius: 1, m: 1 }}>
+            <Grid item xs={5} sx={{ width: '95vw', height: '10vh', backgroundColor: 'primary.main', borderRadius: 1, m: 1 }}>
                 <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', color: 'white' } }}>Usuario radicador:</Typography>
                 <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', color: 'white' } }}>jdoe</Typography>
 
             </Grid>
-            <Grid item  xs={5} sx={{ width: '95vw', height: '10vh', backgroundColor: 'primary.main', borderRadius: 1, m: 1 }}>
-                <Typography  sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', color: 'white' } }}>Nombre radicador: </Typography>
+            <Grid item xs={5} sx={{ width: '95vw', height: '10vh', backgroundColor: 'primary.main', borderRadius: 1, m: 1 }}>
+                <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', color: 'white' } }}>Nombre radicador: </Typography>
                 <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', color: 'white' } }}>John Doe</Typography>
             </Grid>
 
-            <Grid item display='flex' alignItems='center' justifyContent='center'  xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+            <Grid item display='flex' alignItems='center' justifyContent='center' xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
                 <Autocomplete
                     onChange={(event, newValue) => {
                         setProceso(newValue['name']);
@@ -50,6 +90,62 @@ const NuevoDoc = (props) => {
                 <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', mt: 3 } }}>{proceso}</Typography>
 
             </Grid>
+
+            <Grid item display='flex' alignItems='center' justifyContent='center' xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <Autocomplete
+                    onChange={(event, newValue) => {
+                        setLider(newValue['name']);
+                    }}
+                    disablePortal
+                    options={lideres}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Lider aprobador" />}
+                />
+            </Grid>
+            <Grid display='flex' alignItems='center' justifyContent='center' item xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <Typography sx={{ typography: { xs: 'p', sm: 'p', md: 'h5', lg: 'h5', mt: 3 } }}>{lider}</Typography>
+
+            </Grid>
+
+            <Grid item display='flex' alignItems='center' justifyContent='center' xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <Autocomplete
+                    onChange={(event, newValue) => {
+                        //setLider(newValue['name']);
+                    }}
+                    disablePortal
+                    options={documentos}
+                    sx={{ width: 300 }}
+                    renderInput={(params) => <TextField {...params} label="Tipo de documento" />}
+                />
+            </Grid>
+
+            <Grid item display='flex' alignItems='center' justifyContent='center' xs={5} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <Button variant="contained" color="green" sx={{ backgroundColor: 'FCDB25' }}>
+                    <input type="file" />
+                </Button>
+            </Grid>
+
+            <Grid display='flex' alignItems='center' justifyContent='center' item xs={10} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <TextField
+                    fullWidth
+                    label="Justificación"
+                    placeholder="Justificación"
+                    multiline
+                    rows={2}
+                    maxRows={4}
+                    onChange={(event) => {
+                        setJustificacion(event.target.value);
+                    }}
+                />
+            </Grid>
+
+            <Grid display='flex' alignItems='center' justifyContent='center' item xs={10} sx={{ width: '95vw', height: '10vh', m: 1 }}>
+                <Button variant="contained" onClick={() => handleApplication()} color="yellow" sx={{ backgroundColor: 'FCDB25' }}>
+                    Radicar solicitud para nuevos documentos
+                </Button>
+            </Grid>
+
+
 
 
         </Grid>
